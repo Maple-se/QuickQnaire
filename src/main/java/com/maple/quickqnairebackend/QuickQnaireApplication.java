0aee9a4c21daed4debfb55e3ca0e44ac887b78db
郑宗昌 <116479022+Coderzongchang@users.noreply.github.com>
2024-11-27 20:54:00 +0800

    QuickQnaire program initial...

---
+++ src/main/java/com/maple/quickqnairebackend/QuickQnaireApplication.java
@@ -0,0 +1,13 @@
+package com.maple.quickqnairebackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class QuickQnaireApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(QuickQnaireApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/maple/quickqnairebackend/QuickQnaireApplicationTests.java
@@ -0,0 +1,13 @@
+package com.maple.quickqnairebackend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class QuickQnaireApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
